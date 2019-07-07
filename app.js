972ae128370a899b7a0fd37d6a55b195c533dcdb
Gaurav C <cggaurav@gmail.com>
2019-07-07 21:13:48 +0800

    Different statuses

--- app.js
+++ app.js
@@ -7,9 +7,13 @@ const twitter = new Twitter({
 	access_token_secret: process.env.TWITTER_TOKEN_SECRET
 })
 
-const STATUSES = [`Are you scrolling intentionally or mindlessly right now? Friendly reminder to put this away if you don't mean to be here. #mindfulness #reminder`]
+const STATUSES = [
+	`Are you scrolling intentionally or mindlessly right now? Friendly reminder to put this away if you don't mean to be here. #mindfulness #reminder`,
+	`Are you wasting your valuable time? Friendly reminder to put this away if you don't mean to be here. #mindfulness #reminder`,
+	`Are you finding enough signal in your consumption here? Friendly reminder to put this away if you don't mean to be here. #mindfulness #reminder`
+]
 
-twitter.post('statuses/update', { status: STATUSES[0] })
+twitter.post('statuses/update', { status: STATUSES[Math.floor(Math.random() * STATUSES.length)] })
 	.then((s) => {
 		console.log(`Tweeting .. `, s)
 	})
